b4e5dba0329dba34d9a2990f57687bd38c4940c5
Kverde <kverde@yandex.ru>
2022-05-17 00:26:35 +0500

    step 1. added tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Kverde/python-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/Kverde/python-project-lvl3/actions)
+
+[![tests](https://github.com/Kverde/python-project-lvl3/actions/workflows/tests.yml/badge.svg)](https://github.com/Kverde/python-project-lvl3/actions/workflows/tests.yml)
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,5 +1,31 @@
+import requests_mock
+
+import os
+
 from page_loader.page_loader import download
 
+FIXTURES_PATH = './tests/fixtures'
 
-def test_download():
-    pass
+
+def read_file(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+def test_download(tmp_path, requests_mock):
+    page_url = 'https://mysite.com/page-1.htm'
+    filename = 'mysite-com-page-1.html'
+
+    correct_file_content = read_file(os.path.join(FIXTURES_PATH, filename))
+    downloaded_file_path = os.path.join(tmp_path, filename)
+
+    requests_mock.get(page_url, text=correct_file_content)
+
+    download(page_url, tmp_path)
+
+    print(downloaded_file_path)
+
+    assert os.path.exists(downloaded_file_path)
+
+    file_content = read_file(downloaded_file_path)
+    assert file_content == correct_file_content
