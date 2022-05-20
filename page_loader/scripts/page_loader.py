01fbe724f5849549ecb8d8d1007e5a2fcb2e1fcf
Kverde <kverde@yandex.ru>
2022-05-20 20:05:13 +0500

    fix

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -5,16 +5,15 @@ from page_loader.page_loader import download
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('url', help='page URL')
-    parser.add_argument('--output', help='path for save page')
-
-    args = parser.parse_args()
     try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('url', help='page URL')
+        parser.add_argument('--output', help='path for save page')
+
+        args = parser.parse_args()
         download(args.url, args.output)
-    except:
-        pass
-    sys.exit()
+    except:  # noqa: E722
+        sys.exit()
 
 
 if __name__ == '__main__':
